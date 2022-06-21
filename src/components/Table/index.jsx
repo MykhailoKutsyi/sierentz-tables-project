bd8e4f921942ec5c7c6e922094300b10d23a5afd
Mykhailo Kutsyi <91030243+MykhailoKutsyi@users.noreply.github.com>
2022-06-21 17:04:46 +0300

    refactoring, moved from magic numbers

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -9,115 +9,72 @@ import {
   TableRow,
 } from '@mui/material';
 import data from './testData.json';
+import { nanoid } from 'nanoid';
 
 export default function MainTable() {
-  const Kyivska = data[0].Kyivska;
-  const Odeska = data[1].Odeska;
-  const Lvivska = data[2].Lvivska;
-
-  function createData(name, region) {
-    const firstX = region.G[2017].XX.value;
-    const firstY = region.G[2017].YY.value;
-    const firstZ = region.G[2017].ZZ.value;
-
-    const secondX = region.G[2018].XX.value;
-    const secondY = region.G[2018].YY.value;
-    const secondZ = region.G[2018].ZZ.value;
-
-    const thirdX = region.G[2019].XX.value;
-    const thirdY = region.G[2019].YY.value;
-    const thirdZ = region.G[2019].ZZ.value;
-
-    const value = [
-      firstX,
-      firstY,
-      firstZ,
-      secondX,
-      secondY,
-      secondZ,
-      thirdX,
-      thirdY,
-      thirdZ,
-    ];
-    return { name, value };
-  }
-
-  const rows = [
-    createData('Kyivska', Kyivska),
-    createData('Odeska', Odeska),
-    createData('Lvivska', Lvivska),
-  ];
+  const yearsArray = ['2017', '2018', '2019'];
+  const alphabetArray = ['XX', 'YY', 'ZZ'];
 
   const handleClick = () => {
     window.open('popup', 'popup', 'width=1100,height=600');
   };
 
   return (
-    <>
+    <React.Fragment>
       <h1>Main Table</h1>
       <Paper sx={{ width: '100%' }}>
         <TableContainer>
           <Table stickyHeader aria-label="sticky table">
             <TableHead>
               <TableRow>
-                <TableCell rowSpan={2}>regions</TableCell>
-                <TableCell colSpan={3} align="center">
-                  2017
+                <TableCell rowSpan={2} align="center">
+                  Regions
                 </TableCell>
-                <TableCell colSpan={3} align="center">
-                  2018
-                </TableCell>
-                <TableCell colSpan={3} align="center">
-                  2019
-                </TableCell>
+                {yearsArray.map(year => {
+                  return (
+                    <TableCell
+                      key={nanoid(5)}
+                      colSpan={alphabetArray.length}
+                      align="center"
+                    >
+                      {year}
+                    </TableCell>
+                  );
+                })}
               </TableRow>
+
               <TableRow>
-                <TableCell>xx</TableCell>
-                <TableCell>yy</TableCell>
-                <TableCell>zz</TableCell>
-
-                <TableCell>xx</TableCell>
-                <TableCell>yy</TableCell>
-                <TableCell>zz</TableCell>
-
-                <TableCell>xx</TableCell>
-                <TableCell>yy</TableCell>
-                <TableCell>zz</TableCell>
+                {yearsArray.map(() => {
+                  return alphabetArray.map(alphabetValue => {
+                    return (
+                      <React.Fragment key={nanoid(5)}>
+                        <TableCell align="center">{alphabetValue}</TableCell>
+                      </React.Fragment>
+                    );
+                  });
+                })}
               </TableRow>
             </TableHead>
 
             <TableBody>
-              {rows.map(row => {
+              {Object.entries(data).map(([region, values]) => {
                 return (
-                  <TableRow align="center" key={row.name}>
-                    <TableCell component="th">{row.name}</TableCell>
-                    <TableCell align="center" onClick={handleClick}>
-                      {row.value[0]}
-                    </TableCell>
-                    <TableCell align="center" onClick={handleClick}>
-                      {row.value[1]}
-                    </TableCell>
-                    <TableCell align="center" onClick={handleClick}>
-                      {row.value[2]}
-                    </TableCell>
-                    <TableCell align="center" onClick={handleClick}>
-                      {row.value[3]}
-                    </TableCell>
-                    <TableCell align="center" onClick={handleClick}>
-                      {row.value[4]}
-                    </TableCell>
-                    <TableCell align="center" onClick={handleClick}>
-                      {row.value[5]}
-                    </TableCell>
-                    <TableCell align="center" onClick={handleClick}>
-                      {row.value[6]}
-                    </TableCell>
-                    <TableCell align="center" onClick={handleClick}>
-                      {row.value[7]}
-                    </TableCell>
-                    <TableCell align="center" onClick={handleClick}>
-                      {row.value[8]}
-                    </TableCell>
+                  <TableRow key={nanoid(5)}>
+                    <TableCell align="center">{region}</TableCell>
+                    {yearsArray.map(year => {
+                      const yearValues = values.G[year];
+                      return alphabetArray.map(alphabetValue => {
+                        return (
+                          <TableCell
+                            key={nanoid(5)}
+                            align="center"
+                            onClick={handleClick}
+                          >
+                            {yearValues?.[alphabetValue]?.value ?? 0}
+                          </TableCell>
+                        );
+                      });
+                    })}
                   </TableRow>
                 );
               })}
@@ -125,6 +82,6 @@ export default function MainTable() {
           </Table>
         </TableContainer>
       </Paper>
-    </>
+    </React.Fragment>
   );
 }
